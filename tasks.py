6f3a24ca35881205ea75afade17ffe11bfd23a29
Hannah Ouazzane <hannahouazzane@Hannahs-MacBook-Pro.local>
2020-12-19 17:06:44 +0000

    <<hi>>

---
+++ tasks.py
@@ -0,0 +1,12 @@
+# writing a script that sorts dictionary by ascending to descending
+
+
+my_dict={"hannah": 8,
+         "sarah":2,
+         "dad":9,
+         "mum":10}
+
+print(my_dict)
+
+
+# how to sort a dictionary according to the value- modify it?
